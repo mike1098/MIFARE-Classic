21b647f94d6a05a4976a045d1485cee6c744be34
mike1098 <michael.frank@mikefrank.de>
2021-12-05 17:20:26 +0000

    Adds constant for default block data and corrects length of blocks of 1k card to 64

--- MIFARE.py
+++ MIFARE.py
@@ -10,21 +10,36 @@ class Classic1k:
         self.sector = {}
         self.sector_length = 16
         self._sector_trailer_blocks = tuple([i -1 for i in range(4, 65, 4)])
-        self._data_blocks = tuple([i for i in range(0, 48, 1) if i not in self._sector_trailer_blocks])
-        self._sector_trailers = {sector: {'keya': [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF], 'keyb': [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF], 'access': '000000'} for sector in self._sector_trailer_blocks }
+        self._data_blocks = tuple([i for i in range(0, 64, 1) if i not in self._sector_trailer_blocks])
+        self._sector_trailers = {sector: {'keya': [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF], 'keyb': [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF], 'access': [0xFF, 0x07, 0x80]} for sector in self._sector_trailer_blocks }
+        self.DEFAULT_BLOCK_DATA= (0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00) 
 
-    def get_key_a(self, block):
+    def get_key_a(self, block): 
         """
         Returns the Key A from a sector trailer
         """
         #assert (sector is not in self._sector_trailer_blocks), "given sector is not a sector trailer. see _sector_trailer_blocks"
         return self._sector_trailers[block]['keya']
+    
+    def get_key_b(self, block): 
+        """
+        Returns the Key B from a sector trailer
+        """
+        #assert (sector is not in self._sector_trailer_blocks), "given sector is not a sector trailer. see _sector_trailer_blocks"
+        return self._sector_trailers[block]['keyb']
 
     def set_key_a(self, block, keya):
         """
         Set the Key A of a sector trailer
         """
         self._sector_trailers[block]['keya'] = keya
+        return True
+
+    def set_key_b(self, block, keyb):
+        """
+        Set the Key B of a sector trailer
+        """
+        self._sector_trailers[block]['keya'] = keyb
         return True
     
     def get_sector_trailer(self,block):
